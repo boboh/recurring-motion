68dc3dd9d49e84c1bb8cbce75d16f290e3973977
Michael Borosh <borosh@gmail.com>
2016-07-15 15:47:16 -0500

    adding css

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/',function(req,res){
   //res.send("How far did you swim today?");
